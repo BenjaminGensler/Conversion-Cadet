300b8eb7d0f4caa00b2eec47d8c071c43916c393
Benjamin Gensler <117399625+BenjaminGensler@users.noreply.github.com>
2025-05-22 16:12:42 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+// Java program for conversions (Universal Converter)
+// This program allows users to convert between various units of length.
+// -------------------------------------------------------------------
+// Author: Benjamin Gensler
+// Created On: 5/22/2025
+// Last Modified: 5/22/2025
+// Version: 1.0
+// Description: This program was developed to provide a simple and user-friendly
+//              way to convert between units of length such as meters, inches, feet, etc.
+// -------------------------------------------------------------------
+
+// This will include:
+// 1. Length
+
+import java.util.Scanner;
+Scanner scanner = new Scanner(System.in);
+
+system.out.println("Please select from below what information you would like to convert?");
+system.out.println("1 - meters");
+system.out.println("2 - inches");
+system.out.println("3 - feet");
+// put more in later
+
+System.out.println("Please enter first the unit you would like to convert from and then the unit you would like to convert to");
+double name = scanner.nextLine();
+double name2 = scanner.nextLine();
+
+System.out.println("Please enter the number of ${name} you would like to convert");
+double num = scanner.nextDouble();
+
+// Deciding on how best to convert the given metric
