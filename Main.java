d4c97f82b68400bd9dd84d2a8968e6772c38f37d
Benjamin Gensler <117399625+BenjaminGensler@users.noreply.github.com>
2025-05-28 18:02:31 +0000

    Added prefix conversions

--- Main.java
+++ Main.java
@@ -47,6 +47,7 @@ public class Main {
             System.out.println((i + 1) + " - " + units[i]);
         }
 
+
         System.out.println("Please enter the unit you would like to convert from:");
         int var1 = scanner.nextInt() - 1;
         System.out.println("Please enter the unit you would like to convert to:");
@@ -55,8 +56,27 @@ public class Main {
         System.out.println("Please enter the number of" + units[var1] + " you would like to convert");
         double num = scanner.nextDouble();
 
+        //Prefix array
+        // String[] prefixes = {"yocta (y)", "zepto (z)", "atto (a)", "femto (f)", "pico (p)", "nano (n)", "micro (u)", "milli (m)", "centi (c)", "deci (d)", "(No prefix)", "deca (da)", "hecto (h)", "kilo (k)", "mega (M)", "giga (G)", "tera (T)", "peta (P)", "exa (E)", "zetta (Z)", "yotta (Y)"};
+        String[] prefixes = {"yocta (y)", "zepto (z)", "atto (a)", "femto (f)", "pico (p)", "nano (n)", "micro (u)", "milli (m)", "centi (c)", "deci (d)", "(No prefix)", "deca (da)", "hecto (h)", "kilo (k)", "mega (M)", "giga (G)"};
+        // double[] prefixValues = {0.000000000000000001, 0.000000000000001, 0.000000000001, 0.000000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 1000000, 1000000000, 1000000000000, 1000000000000000, 1000000000000000000, 1000000000000000000000, 1000000000000000000000000};
+        double[] prefixValues = {0.000000000000000001, 0.000000000000001, 0.000000000001, 0.000000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 1000000, 1000000000};
+        for(int i = 0; i < prefixes.length; i++) {
+            System.out.println((i + 1) + " - " + prefixes[i]);
+        }
+        System.out.println("Please tell me what the prefix is for the number you entered for " + units[var1] + "(e.g., kilo, mega, etc.)");
+        int prefixPrimary = scanner.nextInt() - 1;
+        System.out.println("Please tell me what the prefix is for the number you would like to convert to " + units[var2] + "(e.g., kilo, mega, etc.)");
+        int prefixSecondary = scanner.nextInt() - 1;
+
+        // Converting the prefix to a multiplier
+        double prefixMultiplier = prefixValues[prefixPrimary] / prefixValues[prefixSecondary];
+
         // Deciding on how best to convert the given metric
         double convertedNum = converter.convert(num, var1, var2);
+
+        // Applying the prefix multiplier to the converted number
+        convertedNum *= prefixMultiplier;
 
         System.out.println("The converted number is: " + convertedNum);
 
